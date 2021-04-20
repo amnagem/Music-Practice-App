812afa8ffa15fe4f43fb0c35b463beea10d1e24f
Angela-Maureen Nagem <angela-maureennagem@Angela-Maureens-MacBook-Air.local>
2021-04-20 11:37:14 -0400

    Basic navigation with router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+const TrackerPage = () => (
+  <div>
+    <h1>Tracker Page</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/tracker' component={TrackerPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends React.Component {
         {
           title: 'Tracker',
           imageUrl: 'http://collections.nmmusd.org/Violins/Amati3423/3423HAmatiViolinSoundholesBlackLG.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'tracker'
         },
         {
           title: 'Library',
           imageUrl: 'https://i.pinimg.com/originals/8d/de/f1/8ddef1dea4edd6df117d5e8dffc565f6.jpg',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'Graph',
           imageUrl: 'https://necmusic.edu/sites/default/files/styles/small_landscape/public/2017-01/Inline4x1_WoodwindKeysABS_2395.jpg?itok=WLdMZjqb',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'Log',
           imageUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/old-piano-keys-jim-hughes.jpg',
-          
-          id: 4
+          id: 4,
+          linkUrl: ''
+
         },
         {
           title: 'Due Dates',
           imageUrl: 'https://i.pinimg.com/originals/9f/96/d8/9f96d855d5f38a2f746c612f6aa0a884.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -42,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
